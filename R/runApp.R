dea38d5fb27344ecd066bd9759f418a180026fee
goushixue <37802226+goushixue@users.noreply.github.com>
2020-11-04 07:59:53 +0000

    Update runApp.R

--- R/runApp.R
+++ R/runApp.R
@@ -10,7 +10,7 @@ startQRseq <- function() {
   # set upload file size limit as 100MB
   options(shiny.maxRequestSize = 1000 * 1024^2, warn = -1, shiny.sanitize.errors = TRUE)
 
-  # source(system.file("shiny", "myApp/global.R", package = "QRseq"), local = T)
+  source(system.file("shiny", "myApp/global.R", package = "QRseq"), local = T)
   addResourcePath(prefix = "www", directoryPath = system.file("shiny", "myApp/www", package = "QRseq"))
   addResourcePath(prefix = "images", directoryPath = system.file("shiny", "myApp/www/images", package = "QRseq"))
 
