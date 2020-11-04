a7f14abcc058959d0af29096598ac25a8fb7992f
goushixue <13610294176@163.com>
2020-11-04 17:14:38 +0800

    update home page

--- inst/shiny/myApp/modules/ui-home-introduction.R
+++ inst/shiny/myApp/modules/ui-home-introduction.R
@@ -14,8 +14,8 @@ fluidPage(
           tags$ul(
             class = "nav",
             tags$li(tags$a("Abstract", href="#abstract")),
-            tags$li(tags$a("Pipeline", href="#pipeline")),
             tags$li(tags$a("Features", href="#features"))
+            # tags$li(tags$a("Features", href="#features"))
           )
         ),
         tags$li(
@@ -43,13 +43,22 @@ fluidPage(
     hr(style = "border-top: 1px solid orange;"),
 
     h3("Abstract", id = "abstract", style = "width:100%; text-align:left;font-family:'Times New Roman', Times, serif;"),
-    p("We propose a web application (QRseq) for exploring and analyzing RNA sequencing data."),
-
-    h3("Pipeline", id = "pipeline", style = "width:100%; text-align:left;font-family:'Times New Roman', Times, serif;"),
-    p("We propose a web application (QRseq) for exploring and analyzing RNA sequencing data."),
+    p("Here we developed QRseq, a R shiny application can easily launched from local web browser for analyzing sequenced or
+      published RNA-seq data. QRseq allows user to upload RNA-seq data from local or to input some keyworks or an accession
+      number of GEO DataSets within the app to start their analysis. This application start from data input, followed by preprocessing
+      data by filtering low expressed genes and poorly reproducible samples, correcting batch effects, normalizing and transforming data,
+      identifying differential expressed genes and other biological patterns, exploring the enrichment of functions, analyzing and visualizing
+      the protein to protein networks or gene regulation networks. QRseq provide a clear analysis flow and an user friendly GUI interface but
+      keep most important parameter of involved functions, which suite for both non-programing experience researchers and expert bioinformatic
+      researchers. User can accomplish a standard RNA-seq analysis in hours depend on the size of their dataset and requires using QRseq.",
+      style = "text-align:justify;font-family:'Times New Roman', Times, serif;"),
 
     h3("Features", id = "features", style = "width:100%; text-align:left;font-family:'Times New Roman', Times, serif;"),
-    p("We propose a web application (QRseq) for exploring and analyzing RNA sequencing data."),
+    p(strong("The main Features was showed below:")),
+    tags$img(src = "images/workflow.tiff",width = "70%"),
+
+    # h3("Features", id = "features", style = "width:100%; text-align:left;font-family:'Times New Roman', Times, serif;"),
+    # p("We propose a web application (QRseq) for exploring and analyzing RNA sequencing data."),
 
     h1("User Guide", id = "data-input", style = "width:100%; text-align:left;font-family:'Times New Roman', Times, serif;"),
     hr(style = "border-top: 1px solid orange;"),
