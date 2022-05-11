b54cc8cd14fb76bef620572fc16d545ed8311a8d
goushixue <13610294176@163.com>
2022-05-11 11:11:17 +0800

    change package name

--- R/runApp.R
+++ R/runApp.R
@@ -1,29 +1,29 @@
-#' Start the QRseq shiny App
+#' Start the QRAP shiny App
 #'
 #' @importFrom dplyr %>%
 #' @importFrom shiny shinyApp runApp addResourcePath
 #' @export
 #'
 
-startQRseq <- function(launch.browser = TRUE) {
+startQRAP <- function(launch.browser = TRUE) {
 
   # set upload file size limit as 100MB
   options(shiny.maxRequestSize = 1000 * 1024^2, warn = -1, shiny.sanitize.errors = TRUE)
 
-  source(system.file("shiny", "myApp/global.R", package = "QRseq"), local = T)
-  addResourcePath(prefix = "www", directoryPath = system.file("shiny", "myApp/www", package = "QRseq"))
-  addResourcePath(prefix = "images", directoryPath = system.file("shiny", "myApp/www/images", package = "QRseq"))
-  # addResourcePath(prefix = "Kegg_dir", directoryPath = system.file("shiny", "myApp/www/Kegg_dir", package = "QRseq"))
+  source(system.file("shiny", "myApp/global.R", package = "QRAP"), local = T)
+  addResourcePath(prefix = "www", directoryPath = system.file("shiny", "myApp/www", package = "QRAP"))
+  addResourcePath(prefix = "images", directoryPath = system.file("shiny", "myApp/www/images", package = "QRAP"))
+  # addResourcePath(prefix = "Kegg_dir", directoryPath = system.file("shiny", "myApp/www/Kegg_dir", package = "QRAP"))
 
-  # kegg_dir <- system.file("shiny", "myApp/www/Kegg_dir", package = "QRseq")
+  # kegg_dir <- system.file("shiny", "myApp/www/Kegg_dir", package = "QRAP")
   # lapply(dir(kegg_dir, full.names = TRUE), function(x){file.remove(x)})
 
   shinyApp(ui = mainUI, server = mainServer) %>% runApp(launch.browser = launch.browser)
 
-  # appDir <- system.file("shiny", "myApp", package = "QRseq")
+  # appDir <- system.file("shiny", "myApp", package = "QRAP")
   #
   # if (appDir == "") {
-  #   stop("Could not find App directory. Try re-installing `QRseq`.", call. = FALSE)
+  #   stop("Could not find App directory. Try re-installing `QRAP`.", call. = FALSE)
   # }
   #
   # shiny::runApp(appDir, display.mode = "normal")
